1dcc5c774a070a3f1df291b7ebdccf0df2c22aab
QuantumK <quantumk9@hotmail.com>
2023-02-15 23:31:24 +0200

    make 3 column footer stackable and with 3 rows

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,76 @@
 import React from "react";
-import { Container, Grid, Image, Segment } from "semantic-ui-react";
+import {
+  Grid,
+  Segment,
+  Image,
+  List,
+  Header,
+  Container,
+} from "semantic-ui-react";
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <>
-      <Grid container centered stackable columns={3}>
-        <Grid.Column>
-          <Segment className="content">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
-            repudiandae temporibus numquam. Eveniet, consectetur? Aperiam
-            voluptatem quis delectus vitae rerum!
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment className="content">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
-            repudiandae temporibus numquam. Eveniet, consectetur? Aperiam
-            voluptatem quis delectus vitae rerum!
-          </Segment>
-        </Grid.Column>
-
-        <Grid.Column>
-          <Segment className="content">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
-            repudiandae temporibus numquam. Eveniet, consectetur? Aperiam
-            voluptatem quis delectus vitae rerum!
-          </Segment>
-        </Grid.Column>
-      </Grid>
+      <Segment vertical style={{ padding: "5em 0em" }}>
+        <Container>
+          <Grid divided stackable>
+            <Grid.Column width={4}>
+              <Grid.Row>
+                <Header as="h4" content="Follow Us" />
+                <List link>
+                  <List.Item as="a">Instagram</List.Item>
+                  <List.Item as="a">Twitter</List.Item>
+                  <List.Item as="a">Facebook</List.Item>
+                </List>
+              </Grid.Row>
+              <Grid.Row>
+                <Header as="h4" content="Restaurant location" />
+                <List link>
+                  <List.Item as="a">Address</List.Item>
+                  <List.Item as="a">1234 Street Name, London, Uk</List.Item>
+                </List>
+              </Grid.Row>
+              <Grid.Row>
+                <Header as="h4" content="Contact Us" />
+                <List>
+                  <List.Item as="a">+4412345678</List.Item>
+                  <List.Item as="a" id="email">
+                    info@reactrestaurant.co.uk
+                  </List.Item>
+                </List>
+              </Grid.Row>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Grid.Row>
+                <Header as="h4" content="Follow Us" />
+                <List link>
+                  <List.Item as="a">Instagram</List.Item>
+                  <List.Item as="a">Twitter</List.Item>
+                  <List.Item as="a">Facebook</List.Item>
+                </List>
+              </Grid.Row>
+              <Grid.Row>
+                <Header as="h4" content="Restaurant location" />
+                <List link>
+                  <List.Item as="a">Address</List.Item>
+                  <List.Item as="a">1234 Street Name, London, Uk</List.Item>
+                </List>
+              </Grid.Row>
+              <Grid.Row>
+                <Header as="h4" content="Contact Us" />
+                <List>
+                  <List.Item as="a">+4412345678</List.Item>
+                  <List.Item as="a" id="email">
+                    info@reactrestaurant.co.uk
+                  </List.Item>
+                </List>
+              </Grid.Row>
+            </Grid.Column>
+            <Grid.Column width={4}>React Stuff</Grid.Column>
+          </Grid>
+        </Container>
+      </Segment>
     </>
   );
 }
