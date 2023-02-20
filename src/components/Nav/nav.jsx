4426f86fef63573f78b06df00a31f4c13d89b43a
CrystalPharma <crystalpharmauk@gmail.com>
2023-02-20 21:48:35 +0000

    edit Home.jsx slider and nav styles

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -1,42 +1,29 @@
-import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 import "./nav.css";
-import { Link } from "react-router-dom";
-import image from "../../assets/image.png";
+import { Link } from 'react-router-dom';
+import image from '../../assets/nonna-logo-light.png'
 
 function MyNav() {
-  return (
-    <Navbar collapseOnSelect expand="lg" className="space">
-      <div className="space-logo">
-        <img
-          src={image}
-          alt="logo"
-          style={{ position: "fixed", height: "10" }}
-        />
-      </div>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse className="pages">
-        <Nav className="ml-auto clear">
-          <Link to="/">
-            <Nav.Link style={{ color: "#469597", listStyle: "none" }} as="div">
-              Home{" "}
-            </Nav.Link>
-          </Link>
-          <Link to="/menu">
-            <Nav.Link style={{ color: "#469597" }} as="div">
-              Menu
-            </Nav.Link>
-          </Link>
-          <Link to="/order now">
-            <Nav.Link style={{ color: "black", fontFamily: "roboto" }} as="div">
-              Order Now
-            </Nav.Link>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+    return (
+
+        <Navbar collapseOnSelect expand="lg"  className="space">
+            <div className='space-logo'>
+                
+                <img src ={image} alt="logo"/>
+            </div>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse classeName ='pages'>
+                <Nav className="ml-auto clear">
+                    <Link></Link>
+                    <Link to="/home"><Nav.Link  class='nav-link' style={{ color: "white", listStyle:'none', fontSize:'10 rem'}} as="div">Home </Nav.Link></Link>
+                    <Link to="/menu"><Nav.Link class='nav-link' style={{ color: "white"}} as="div">Menu</Nav.Link></Link>
+                    <Link to="/order now"><Nav.Link className="ui yellow button medium nav-link" as="div">Contact us</Nav.Link></Link> 
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
 
 export default MyNav;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,102 +1,66 @@
 import React from "react";
-import Sliderpage from "../components/Slider/Slider.js";
-import { Segment, Container, Image } from "semantic-ui-react";
+import Sliderpage from '../../src/components/Slider/slider';
+import { Segment, Container } from 'semantic-ui-react';
+
+
 export default function Home() {
   return (
     <div>
-      {/* <Segment basic style={{ fontFamily: 'Gloock'}}>
-       <h1 class="one column stackable ui grid centered huge header" style={{ fontFamily: 'Corinthia' }}><u>Nonna's</u></h1>
-        <h2 class="one column stackable ui grid centered">Mediterranean Bistro</h2>
-        <h3 class="one column stackable ui grid centered">Welcome to Nonnas Bistro -  your one-stop-shop for all things delicious and Mediterranean! Our family-owned and operated takeaway serves up mouthwatering dishes that are sure to satisfy your cravings</h3>
-        <button class="ui inverted teal basic button">Explore More</button>
-       
-          <div class="two column stackable ui grid">
-            <div class="row">
-              <img src={Top} style={{ display: 'flex', position: 'relative', width: '100%' ,opacity: '90%', zIndex: '0', justifyContent: 'center'}}/>
-           
-            </div>
-          </div>
-          <div className="ui diver"></div>
-          <Container text>
-          <div class="six wide computer column" style={{ fontFamily: 'Gloock' }}>
-            <p>Nonna's Mediterranean Bistro - we are a family-run business that prides itself on delivering authentic Mediterranean cuisine using fresh and high-quality ingredients. Our recipes have been       passed down from generations and are rooted in the traditions of the Mediterranean.
-            </p>
-          </div>
-          
-        </Container>
-        </Segment> */}
-      <Segment basic style={{ fontFamily: "Glook", color: "#FFF979" }}>
+      <Segment basic style={{ fontFamily: 'Glook', color: '#EFCD77' }}>
         <Container>
-          <h1
-            className="one column stackable ui grid centered"
-            style={{ fontFamily: "Corinthia" }}
-          >
-            <b>
-              <u>Nonna</u>
-            </b>
-          </h1>
-          <h2
-            className="one column stackable ui grid centered"
-            style={{ fontFamily: "Corinthia" }}
-          >
-            Mediterranean Bistro
-          </h2>
-          <br />
-          <br />
+          <h1 className="one column stackable ui grid centered" style={{ fontFamily: 'Corinthia' }}><b><u>Nonna's</u></b></h1>
+          <h2 className="one column stackable ui grid centered" style={{ fontFamily: 'Corinthia' }}>Mediterranean Bistro</h2>
+          <br /><br />
           <Container>
-            <h3 className="one column stackable ui grid centered">
-              {" "}
-              Welcome to Nonnas Bistro - your one-stop-shop for all things
-              delicious and Mediterranean! Our family-owned and operated
-              takeaway serves up mouthwatering dishes that are sure to satisfy
-              your cravings
-            </h3>
-            <br />
-            <br />
+            <h3 className="one column stackable ui grid centered"> Welcome to Nonna's Bistro -  your one-stop-shop for all things delicious and Mediterranean! Our family-owned and operated takeaway serves up mouthwatering dishes that are sure to satisfy your cravings</h3>
+            <br /><br />
           </Container>
           <div className="one column stackable ui grid">
             <div className="row">
               <div>
-                <div
-                  className="six wide computer column centered"
-                  style={{ display: "flex", justifyContent: "center" }}
-                >
-                  <a style={{ position: "relative", zIndex: "1" }} href="#">
-                    <button className="ui yellow  basic button">
-                      Explore More
-                    </button>
-                  </a>
+                <div className="six wide computer column centered" style={{ display: 'flex', justifyContent: 'center' }}>
+                  <a style={{ position: 'relative', zIndex: '1' }} href='#'><button class="ui inverted yellow button">Explore More</button></a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="ui divider"></div>
-          <div className="one column stackable ui grid centered">
-            <h1 style={{ fontFamily: "Corinthia" }}> About</h1>
-            <h3>
-              {" "}
-              Nonna's Mediterranean Bistro - we are a family-run business that
-              prides itself on delivering authentic Mediterranean cuisine using
-              fresh and high-quality ingredients. Our recipes have been passed
-              down from generations and are rooted in the traditions of the
-              Mediterranean.
-            </h3>
-            <div></div>
-          </div>
         </Container>
+        <br />
       </Segment>
       <div>
-        <Sliderpage />
-        <br /> <br />
+        <div className="ui grid" style={{ color: 'white', backgroundColor: '#5A1B0E' }}>
+          <div className="one column stackable ui grid centered">
+            <div className="ui divider"></div>
+            <div className="sixteen wide column">
+              <Container>
+                <h1 className="one column stackable ui grid centered" style={{ fontFamily: 'Corinthia' }}> About</h1>
+                <h3 className="one column stackable ui grid centered"> Nonna's Mediterranean Bistro - we are a family-run business that prides itself on delivering authentic Mediterranean cuisine using fresh and high-quality ingredients. Our recipes have been   passed down from generations and are rooted in the traditions of the Mediterranean.</h3>
+              </Container>
+              <br />
+            </div>
+            <div className="ui divider"></div>
+            <Container>
+              <Sliderpage />
+            </Container>
+          </div>
+        </div>
+        <div>
+        </div>
       </div>
-      <div
-        className="ui one column stackable ui grid centered"
-        style={{ marginTop: "100" }}
-      >
-        <button className="ui basic yellow button">Nonnas' Menu</button>
-        <button className="ui basic yellow button">Book Table</button>
+      <div className="ui grid" style={{ color: 'white', backgroundColor: '#5A1B0E' }}>
+        <div className="one column stackable ui grid centered">
+          <div className="sixteen wide column">
+            <Container>
+              <h1 className="one column stackable ui grid centered" style={{ fontFamily: 'Corinthia' }}>To know more ...</h1>
+              <div className="ui one column stackable ui grid centered" style={{ display: "flex", justifyContent: 'center' }}>
+                <button className="ui inverted yellow button">Nonna's Menu</button>
+                <button className="ui inverted yellow button">Book Table</button>
+              </div>
+            </Container>
+            <br />
+          </div>
+        </div>
       </div>
-      <div className="ui divider"></div>
     </div>
-  );
+  )
 }
