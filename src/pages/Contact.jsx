25c39cb5258bafa0c56496791601ba8831a4805f
QuantumK <quantumk9@hotmail.com>
2023-02-21 19:15:11 +0000

    add wrapper and content in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,17 @@ import Slider from "../components/Slider/Slider";
 
 export default function Order() {
   return (
-    <div>
+    <div className="wrapper">
       <Slider />
+      <h2 className="contact-header">Contact Us</h2>
+      <div className="contact-wrapper">
+        <div className="left-section">
+          <ContactInfo />
+        </div>
+        <div className="right-section">
+          <ContactForm />
+        </div>
+      </div>
     </div>
   );
 }
