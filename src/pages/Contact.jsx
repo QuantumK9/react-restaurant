7de609182b05ef7f2b52255bd59a90e23873dfa3
QuantumK <quantumk9@hotmail.com>
2023-02-21 20:42:04 +0000

    add hero to contact page, remove carousel and fix styles to match home page and be responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,18 +3,57 @@ import Slider from "../components/Slider/Slider";
 import ContactInfo from "../components/ContactInfo/ContactInfo";
 import ContactForm from "../components/ContactForm/ContactForm";
 import "./Contact.css";
+import { Segment, Container } from "semantic-ui-react";
 
 export default function Order() {
   return (
     <div className="wrapper">
-      <Slider />
-      <h2 className="contact-header">Contact Us</h2>
-      <div className="contact-wrapper">
-        <div className="left-section">
-          <ContactInfo />
-        </div>
-        <div className="right-section">
-          <ContactForm />
+      {/* <Slider /> */}
+      <Segment
+        basic
+        style={{ fontFamily: "Glook", color: "#EFCD77" }}
+        className="contact-hero"
+      >
+        <Container>
+          <h1 className="one column stackable ui grid centered">Contact Us</h1>
+          <h2
+            className="one column stackable ui grid centered"
+            style={{ fontFamily: "Corinthia" }}
+          ></h2>
+          <br />
+          <br />
+          <Container>
+            <h3 className="one column stackable ui grid centered">
+              {" "}
+              We'd love to hear from you !
+            </h3>
+            <br />
+            <br />
+          </Container>
+          <div className="one column stackable ui grid">
+            <div className="row">
+              <div>
+                <div
+                  className="six wide computer column centered"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </Container>
+        <br />
+      </Segment>
+      <div className="info-form-wrapper">
+        <div className="contact-wrapper">
+          <div className="left-section">
+            <ContactInfo />
+          </div>
+          <div className="right-section">
+            <ContactForm />
+          </div>
         </div>
       </div>
     </div>
