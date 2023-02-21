4bb8730ad70668d0a5d62d55b00834dc2a82e232
QuantumK <quantumk9@hotmail.com>
2023-02-21 22:06:42 +0000

    install swiper and react-hook-form and change navbar to reroute to contact page instead of order page when contact button pressed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MyNav from './components/Nav/nav';
-import Gallery from './container/Gallery/Gallery';
-import Home from './pages/Home';
-import Menus from './container/Menus/BestMenu';
-import Order from "./pages/Order";
+import MyNav from "./components/Nav/nav";
+import Gallery from "./container/Gallery/Gallery";
+import Home from "./pages/Home";
+import Menus from "./container/Menus/BestMenu";
+import Contact from "./pages/Contact";
 import Footer from "./components/Footer/Footer.jsx";
-import './App.css';
-
+import "./App.css";
 
 const App = () => (
   <div>
-    <Router >
+    <Router>
       <MyNav />
-      <Switch >
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/menus" component={Gallery} />
-        <Route exact path="/order" component={Order} />
+        <Route exact path="/Contact" component={Contact} />
         <Route exact path="/menu" component={Menus} />
-      </Switch >
+      </Switch>
       <Footer />
-    </Router >
+    </Router>
   </div>
 );
-
-
-
 
 export default App;
