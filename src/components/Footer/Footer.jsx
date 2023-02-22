4256e52363a4378bc79456ac850a3bafc2520a31
uklivewire <rickybryans0@gmail.com>
2023-02-22 00:16:38 +0000

    update to footer css styling, minor adjustments to footer jsx inline with styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,73 +14,38 @@ export default function Footer() {
     <div className="footer-wrapper">
       <Segment vertical style={{ padding: "5em 0em" }}>
         <Container>
-          <Grid divided stackable>
-            <Grid.Column width={4}>
-              <Grid.Row>
-                <Header as="h4" content="Follow Us" />
-                <List link className="text-centered">
+         <Grid divided stackable>
+          <Grid.Row>
+            <Grid.Column width={4} textAlign="center">
+              <Header as="h4" content="Follow Us" />
+                <List link className="text-centered follow-us-list">
                   <List.Item as="a">Instagram</List.Item>
                   <List.Item as="a">Twitter</List.Item>
                   <List.Item as="a">Facebook</List.Item>
                 </List>
-              </Grid.Row>
-              <Grid.Row>
-                <Header as="h4" content="Restaurant location" />
+            </Grid.Column>
+            <Grid.Column width={4} textAlign="center">
+              <Header as="h4" content="Restaurant location" />
                 <List link>
                   <List.Item as="a">Address</List.Item>
                   <List.Item as="a">1234 Street Name, London, Uk</List.Item>
-                </List>
-              </Grid.Row>
-              <Grid.Row>
-                <Header as="h4" content="Contact Us" />
-                <List>
+               </List>
+            </Grid.Column>
+            <Grid.Column width={4} textAlign="center">
+             <Header as="h4" content="Contact Us" />
+              <List>
                   <List.Item as="a">+4412345678</List.Item>
-                  <List.Item as="a" id="email">
-                    info@reactrestaurant.co.uk
-                  </List.Item>
-                </List>
-              </Grid.Row>
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <Grid.Row>
-                <Header as="h4" content="Follow Us" />
-                <List link>
-                  <List.Item as="a">Instagram</List.Item>
-                  <List.Item as="a">Twitter</List.Item>
-                  <List.Item as="a">Facebook</List.Item>
-                </List>
-              </Grid.Row>
-              <Grid.Row>
-                <Header as="h4" content="Restaurant location" />
-                <List link>
-                  <List.Item as="a">Address</List.Item>
-                  <List.Item as="a">1234 Street Name, London, Uk</List.Item>
-                </List>
-              </Grid.Row>
-              <Grid.Row>
-                <Header as="h4" content="Contact Us" />
-                <List>
-                  <List.Item as="a">+4412345678</List.Item>
-                  <List.Item as="a" id="email">
-                    info@reactrestaurant.co.uk
-                  </List.Item>
-                </List>
-              </Grid.Row>
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <Grid.Row>
-                <Header as="h4" content="Developed By" />
-                <List>
-                  <List.Item as="a">Crystal</List.Item>
-                  <List.Item as="a">Ricky</List.Item>
-                  <List.Item as="a">Adama</List.Item>
-                  <List.Item as="a">Iraklis</List.Item>
-                </List>
-              </Grid.Row>
-            </Grid.Column>
-          </Grid>
-        </Container>
-      </Segment>
-    </div>
-  );
+                  <List.Item as="a" id="email">info@reactrestaurant.co.uk</List.Item>
+             </List>
+          </Grid.Column>
+        </Grid.Row>
+  <Grid.Row id="developed-by">
+    <Grid.Column width={16} textAlign="center">Developed by Crystal, Ricky, Adama, Iraklis</Grid.Column>
+  </Grid.Row>
+</Grid>
+
+      </Container>
+     </Segment>
+   </div>
+ );
 }
